910462b075e23b277548cf71bee776be7e8804a0
Rahull025 <mahatorahul025@gmail.com>
2019-08-05 12:23:59 +0530

    Update Q21.java

--- Q21.java
+++ Q21.java
@@ -1,43 +1,35 @@
 import java.util.*;
 
 public class Amicable {
-
-	int isAmicable(int n) {
-		
-		int sum1=0,sum2=0;
+	
+	
+	public static int summation(int n)
+	{
+		int sum=0;
 		for(int i=1;i<n;i++)
 		{
 			if(n%i==0)
-			{
-				sum1=sum1+i;
-			}
+			sum+=i;
 		}
-		for(int i=1;i<sum1;i++)
-		{
-			if(sum1%i==0)
-			{
-				sum2=sum2+i;
-			}
-		}
-		if(sum2==n&&sum1!=sum2)
-			return sum2;
-		else
-		    return 0;
+		return sum;
+	}
+
+	 public static boolean isAmicable(int n) {
+		
+		int t=summation(n);
+		if(t!=n && summation(t)==n)
+		return true; else return false;
 	}
 	
 	public static void main(String args[])
 	{
-		int fin=0;
-		Amicable ob=new Amicable();
-		
+		int sum=0;
 		for(int i=1;i<10000;i++)
 		{
-			if(ob.isAmicable(i)!=0)
-			{
-			fin=fin+ob.isAmicable(i);
-			}
+			if(isAmicable(i))
+			sum+=i;
 		}
-		System.out.println(fin);
+		System.out.println(sum);
 	}
 }
       
