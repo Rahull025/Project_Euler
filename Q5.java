8818b3001c9cad5774ae06aa1457584ba0542c19
Rahull025 <mahatorahul025@gmail.com>
2019-08-05 12:22:51 +0530

    Update Q5.java

--- Q5.java
+++ Q5.java
@@ -1,32 +1,27 @@
-public class Sml_Multiple
+import java.util.*;
+class SmlMul
 {
-
-    public static void main(String[] args) {
-    	
-    	Sml_Multiple ob=new Sml_Multiple();
-        System.out.println(ob.findSmallestMultiple(20));
-    }
-    
-    long findSmallestMultiple(int n) {
-        for (long i = n; i <= factorial(n); i += n) {
-            if (isMultiple(i, n)) return i;
-        }
-        return -1;
-    }
-    
-    boolean isMultiple(long x, int n) {
-        for (int i = 1; i < n; i++)
-        {
-            if (x % i!= 0) 
-            return false;
-        }
-        return true;
-    }
-    
-    long factorial (long n) {
-        if (n > 1)
-        return n * factorial(n - 1);
-        else if (n >= 0) return 1;
-        else return -1;
-    }
+	static long gcd(long a,long b)
+	{
+		if(b==0)
+		{
+			return a;
+		}
+		return(gcd(b,a%b));
+	}
+	
+	static long lcm(long n)
+	{
+		long r=1;
+		for(int i=2;i<=n;i++)
+		{
+			r=(r*i)/gcd(r,i);
+		}
+		return r;
+	}
+	
+	public static void main(String args[])
+	{
+		System.out.println(lcm(20));
+	}
 }
