52ec0b322b4bec196fb71643a66c8e2ddc49500f
Rahull025 <mahatorahul025@gmail.com>
2019-08-05 18:52:58 +0530

    Update Q3.java

--- Q3.java
+++ Q3.java
@@ -1,34 +1,17 @@
-class Q3
+import java.util.*;
+class LrgstPrime
 {
-   static long smlFactor(long n)
-	{
-		long end=(long)Math.sqrt(n);
-		for(int i=2;i<=end;i++)
-		{
-			if(n%i==0)
-			return i;
-		}
-		return n;
-	}
-	
-	public static long calc(long n)
-	{
-		while(true)
-		{
-			long p=smlFactor(n);
-			if(p<n)
-			{
-				n/=p;
-			}
-			else 
-			return n;
-		 }
-	}
 	
 	public static void main(String args[])
 	{
 		long n=600851475143L;
-		System.out.println(calc(n));
-		
+		for(long i=3;i<n/2;i+=2)
+		{
+			if(n%i==0)
+			{
+				n/=i;
+			}
+		}
+		System.out.println(n);
 	}
 }
